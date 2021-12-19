97a3603fdeeac315c9580d7d840fe2f910097381
kykim00 <jeongsaeki@likelion.org>
2021-12-20 00:47:45 +0900

    :sparkles: : Add wines

---
+++ src/pages/wines/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Wines: NextPage = () => {
+  return (
+    <div>
+      <h1>Wines</h1>
+    </div>
+  );
+};
+
+export default Wines;
---
+++ src/pages/wines/whites.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const White: NextPage = () => {
+  return (
+    <div>
+      <h1>Whites</h1>
+    </div>
+  );
+};
+
+export default White;
