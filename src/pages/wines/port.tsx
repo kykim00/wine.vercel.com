47c21a3543f653768b84a197c9c4442414613c9c
kykim00 <jeongsaeki@likelion.org>
2021-12-22 01:58:32 +0900

    :zap: : Wine abstraction

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,23 +1,23 @@
 import type { NextPage } from "next";
 import { Wine } from "../../types/Wine";
 import { useWineData } from "../../hooks/useWineData";
-
+import { Error, Loading, WineCard } from "../../components";
 const Port: NextPage = () => {
+  const name = "port";
   const { data, error } = useWineData("port");
 
-  if (error) return <div>Failed to Loading...</div>;
-  if (!data) return <div>...Loading</div>;
+  if (error) return <Error />;
+  if (!data) return <Loading />;
   return (
     <div>
       <h1>Wine</h1>
       <main>
         {data.map((wineData: Wine) => {
-          const { id, wine, winery } = wineData;
           return (
-            <div key={`port-wine-list-${id}`}>
-              <h1>{wine}</h1>
-              <p>{winery}</p>
-            </div>
+            <WineCard
+              key={`port-wine-list-${wineData.id}`}
+              wineData={wineData}
+            ></WineCard>
           );
         })}
       </main>
