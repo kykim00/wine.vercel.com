acdbccc6df2c52213a9a2ad7364939fa746dc881
kykim00 <jeongsaeki@likelion.org>
2021-12-22 01:38:10 +0900

    :sparkles: : Wine API 연동

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from "next";
+import { Wine } from "../../types/Wine";
+import { useWineData } from "../../hooks/useWineData";
 
 const Port: NextPage = () => {
+  const { data, error } = useWineData("port");
+
+  if (error) return <div>Failed to Loading...</div>;
+  if (!data) return <div>...Loading</div>;
   return (
     <div>
-      <h1>Port</h1>
+      <h1>Wine</h1>
+      <main>
+        {data.map((wineData: Wine) => {
+          const { id, wine, winery } = wineData;
+          return (
+            <div key={`port-wine-list-${id}`}>
+              <h1>{wine}</h1>
+              <p>{winery}</p>
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
 };
